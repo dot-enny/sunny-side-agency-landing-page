49a91a04fbc36681c6a77787b204ee78f4ff86cb
OlaniyanEniola <olaniyaneniola0225@gmail.com>
2022-12-24 21:14:14 +0100

    added animation to mobile nav

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const burger = document.querySelector('#burger');
 const menu = document.querySelector('#menu');
 const headerTitle = document.querySelector('.header-title');
-const arrow = document.querySelector('.arrow')
+const arrow = document.querySelector('.arrow');
 
 
 burger.addEventListener ('click', () => {
-    burger.classList.toggle('opac')
-    menu.classList.toggle('hidden');
+    burger.classList.toggle('opac');
+    menu.classList.toggle('menu-in');    
     headerTitle.classList.toggle('hidden');
     arrow.classList.toggle('hidden');
-    if (menu.classList.contains('hidden')) {
+    if (menu.classList.contains('menu-in')) {
         headerTitle.classList.remove('hidden');
         arrow.classList.remove('hidden')
     }
-})
+});
+
